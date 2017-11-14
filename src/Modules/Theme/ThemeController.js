1ad774ca60d9654beb91544ba38989a1985cd042
Олег Евзрезов <mail.ru>
2017-11-14 18:47:07 +0300

    Создание связи темы и пользователя.

--- src/Modules/Theme/ThemeController.js
+++ src/Modules/Theme/ThemeController.js
@@ -1,5 +1,17 @@
 import generateControllers from '../../Core/generateControllers';
 
 import Theme from '../../Schemas/Theme';
+import UserTheme from '../../Schemas/UserTheme';
 
-export default generateControllers(Theme);
+export default generateControllers(Theme, {
+    createOne: async (req, res, next) => {
+        try {
+            let theme = await Theme.create(req.body);
+            let userTheme = await UserTheme.create({user_id: req.user._id, theme_id: theme._id});
+
+            res.status(200).json(theme)
+        } catch (error) {
+            next(error)
+        }
+    }
+});
