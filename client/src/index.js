4296a388e2878fb32fba47f8a008fd37e15bdeeb
Oleg Evz <shelliro@mail.ru>
2017-11-15 21:30:41 +0300

    Правки

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
-import promiseMiddleware from 'redux-promise-middleware';
 import reduxThunk from 'redux-thunk'
 
 import 'materialize-css/dist/css/materialize.min.css';
@@ -10,8 +9,7 @@ import App from './app';
 import reducers from './reducers'
 
 const store = createStore(reducers, {},
-    applyMiddleware(reduxThunk),
-    applyMiddleware(promiseMiddleware()),
+    applyMiddleware(reduxThunk)
 );
 
 ReactDOM.render(
